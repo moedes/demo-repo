075c8dbf48de9d83ae0adcb1eb306eb4bfc792e0
Jerry Mozes <jerry@puppet.com>
2020-10-16 09:26:27 -0600

    Add comments

--- security/manifests/cisregkeys.pp
+++ security/manifests/cisregkeys.pp
@@ -1,11 +1,17 @@
+#Manage CIS Windows registry keys
+
 class security::cisregkeys {
 
-   registry_key {'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Explorer':
+  #CIS Security Control 18.9.30.1 Turn off Data Execution Prevention for Explorer set to Disabled 
+  registry_key {'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Explorer':
       ensure       => present,
       purge_values => true,
-   }
+  }
 
-   registry_value {'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Explorer\NoDataExecutionPrevention':
+  registry_value {'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Explorer\NoDataExecutionPrevention':
       type   => 'dword',
       data   => '0',
-   }
+  }
+  ################################################################################################
+
+}
